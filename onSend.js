466a2a2bf2dc8450163686543da2dad6a678d02c
vikash3pandey-sys <vikash3.pandey@paytm.com>
2026-02-16 08:03:11 +0530

    Update onSend.js

--- onSend.js
+++ onSend.js
@@ -1,11 +1,9 @@
 Office.onReady();
 
-var dialog;
-
 function checkExternalRecipients(event) {
     var item = Office.context.mailbox.item;
 
-// 1. Load Dynamic Trusted Domains
+    // 1. Load Dynamic Trusted Domains
     var savedDomains = Office.context.roamingSettings.get("TrustedDomains");
     var defaultDomains = [
         "paytmpayments.com", "paytmmoney.com", "paytminsurance.co.in", 
@@ -24,6 +22,14 @@ function checkExternalRecipients(event) {
     ];
     var trustedDomains = savedDomains ? JSON.parse(savedDomains) : defaultDomains;
 
+    item.to.getAsync(function(result) {
+        if (result.status !== Office.AsyncResultStatus.Succeeded) {
+            event.completed({ allowEvent: true }); return;
+        }
+
+        var recipients = result.value;
+        var externalEmails = [];
+
         // 2. Check logic
         for (var i = 0; i < recipients.length; i++) {
             var email = recipients[i].emailAddress.toLowerCase();
@@ -33,76 +39,36 @@ function checkExternalRecipients(event) {
                     isSafe = true; break;
                 }
             }
-            if (!isSafe) { 
-                externalEmails.push(email); // Save the bad email
-            }
+            if (!isSafe) { externalEmails.push(email); }
         }
 
+        // 3. Decision Logic
         if (externalEmails.length === 0) {
             // Internal Only -> Send Silently
             event.completed({ allowEvent: true });
         } else {
-            // External Found -> Prepare URL with the list of external emails
-            var encodedEmails = encodeURIComponent(externalEmails.join(","));
-            var url = "https://vikash3pandey-sys.github.io/outlook-alerts/warning.html?ext=" + encodedEmails;
+            // External Found -> Trigger Soft Block
+            item.loadCustomPropertiesAsync(function (propResult) {
+                var props = propResult.value;
+                var warningStatus = props.get("WarningBypass"); 
 
-            Office.context.ui.displayDialogAsync(url, { height: 50, width: 40, displayInIframe: true },
-                function (asyncResult) {
-                    if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-                        event.completed({ allowEvent: false, errorMessage: "Security Check Failed." });
-                    } else {
-                        dialog = asyncResult.value;
-                        
-                        dialog.addEventHandler(Office.EventType.DialogMessageReceived, function(arg) {
-                            dialog.close(); 
-                            
-                            if (arg.message === "allow") {
-                                // SEND TO EVERYONE
-                                event.completed({ allowEvent: true });
-                            } else if (arg.message === "cancel") {
-                                // CANCEL SEND
-                                event.completed({ allowEvent: false });
-                            } else if (arg.message === "remove_and_send") {
-                                // SCRUB EXTERNALS AND SEND
-                                removeExternalsAndSend(item, trustedDomains, event);
-                            }
+                if (warningStatus === "yes") {
+                    // User clicked Send AGAIN -> Allow it
+                    props.remove("WarningBypass");
+                    props.saveAsync(function() {
+                         event.completed({ allowEvent: true });
+                    });
+                } else {
+                    // FIRST CLICK -> Block and show Native Warning Banner
+                    props.set("WarningBypass", "yes");
+                    props.saveAsync(function() {
+                        event.completed({ 
+                            allowEvent: false, 
+                            errorMessage: "⚠️ EXTERNALS FOUND: " + externalEmails.join(", ") + ". Click Send again to allow, or use the PayTM Side Panel to remove them." 
                         });
-                    }
+                    });
                 }
-            );
-        }
-    });
-}
-
-// Helper 1: Removes externals from "To" and "CC" and sends the email
-function removeExternalsAndSend(item, trustedDomains, event) {
-    item.to.getAsync(function(resTo) {
-        var safeTo = filterSafe(resTo.value, trustedDomains);
-        item.to.setAsync(safeTo, function() { 
-            item.cc.getAsync(function(resCc) {
-                var safeCc = filterSafe(resCc.value, trustedDomains);
-                item.cc.setAsync(safeCc, function() {
-                    event.completed({ allowEvent: true });
-                });
             });
-        });
-    });
-}
-
-// Helper 2: Keeps only trusted emails
-function filterSafe(recipients, trustedDomains) {
-    if (!recipients) return [];
-    var safeList = [];
-    
-    for (var i = 0; i < recipients.length; i++) {
-        var email = recipients[i].emailAddress.toLowerCase();
-        var isSafe = false;
-        for (var j = 0; j < trustedDomains.length; j++) {
-            if (email.indexOf("@" + trustedDomains[j]) > -1) {
-                isSafe = true; break;
-            }
         }
-        if (isSafe) { safeList.push(recipients[i]); }
-    }
-    return safeList;
+    });
 }
